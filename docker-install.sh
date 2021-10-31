d77fbc3b9c91b282c3c1612711df846bc4880f34
Shubham Tatvamasi <shubhamtatvamasi@gmail.com>
2021-10-31 08:56:57 +0530

    Update docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -21,8 +21,7 @@ sudo apt install -y docker-ce docker-ce-cli containerd.io
 
 sudo usermod -aG docker $USER
 
-LATEST_RELEASE=$(curl -L -s -H 'Accept: application/json' https://github.com/docker/compose/releases/latest)
-LATEST_VERSION=$(echo $LATEST_RELEASE | sed -e 's/.*"tag_name":"\([^"]*\)".*/\1/')
+LATEST_VERSION=$(curl -s https://github.com/docker/compose/releases/latest | grep -o "v[0-9]\.[0-9]*\.[0-9]*")
 sudo curl -L "https://github.com/docker/compose/releases/download/$LATEST_VERSION/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
 sudo chmod +x /usr/local/bin/docker-compose
